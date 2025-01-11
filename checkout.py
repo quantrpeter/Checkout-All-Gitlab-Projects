69a77e76d48ccaee3183cd35415785e80539b652
Peter <peter@quantr.hk>
2025-01-11 13:29:10 +0800

    dev

---
+++ checkout.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+token = 
+pageSize = 100
+pageNo = 1
+size = pageSize
+
+projects = []
+
+while size == pageSize:
+    url = (
+        "https://gitlab.com/api/v4/projects?private_token="
+        + token
+        + "&membership=true&order_by=name&per_page="
+        + str(pageSize)
+        + "&page="
+        + str(pageNo)
+    )
+    print(url)
+    response = requests.get(url, verify=False)
+    # print(response.content)
+    j = json.loads(response.content)
+    for x in range(len(j)):
+        projects.append({"name": j[x]["name"], "url": j[x]["http_url_to_repo"]})
+        # print(str(x) + " = " + j[x]["name"])
+    size = len(j)
+    pageNo += 1
+
+for x in range(len(projects)):
+    print(str(x) + " = " + projects[x]['name'] + "\t=\t" + projects[x]['url'])
