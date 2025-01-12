c93f64b8b2149f44729abb4d50ae0722727c1885
Peter <peter@quantr.hk>
2025-01-12 22:45:55 +0800

    dev

--- checkout.py
+++ checkout.py
@@ -1,10 +1,26 @@
 import requests
 import json
 import urllib3
+import argparse
+import os
+
+parser = argparse.ArgumentParser(
+    prog="checkout.py",
+    description="checkout all gitlab projects",
+    epilog="Text at the bottom of help",
+)
+parser.add_argument("--username", help="Gitlab username")
+parser.add_argument("--token", help="Gitlab api token")
+args = parser.parse_args()
+
+if args.token == None:
+    parser.print_help()
+    exit(1)
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-token = 
+token = args.token
+username = args.username
 pageSize = 100
 pageNo = 1
 size = pageSize
@@ -25,10 +41,34 @@ while size == pageSize:
     # print(response.content)
     j = json.loads(response.content)
     for x in range(len(j)):
-        projects.append({"name": j[x]["name"], "url": j[x]["http_url_to_repo"]})
+        projects.append(
+            {
+                "name": j[x]["name"],
+                "path": j[x]["path"],
+                "url": j[x]["http_url_to_repo"],
+            }
+        )
         # print(str(x) + " = " + j[x]["name"])
+        # print(j[x])
     size = len(j)
     pageNo += 1
 
 for x in range(len(projects)):
-    print(str(x) + " = " + projects[x]['name'] + "\t=\t" + projects[x]['url'])
+    print(
+        str(x)
+        + " = "
+        + projects[x]["name"]
+        + "\t=\t"
+        + projects[x]["path"]
+        + "\t=\t"
+        + projects[x]["url"]
+    )
+    if os.path.isdir(projects[x]["path"]):
+        print(projects[x]["path"] + " existed")
+        os.system("cd " + str(projects[x]["path"]) + "; git pull")
+    else:
+        url = projects[x]["url"]
+        url = url.replace(
+            "https://", "https://" + str(username) + ":" + str(token) + "@"
+        )
+        os.system("git config user.email peter@quantr.hk;git clone " + url)
